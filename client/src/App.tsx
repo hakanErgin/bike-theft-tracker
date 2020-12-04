c842000cabf3d577a29bc224a0e25362423d8117
hakanErgin <hakan.ergin@gmail.com>
2020-12-04 17:15:14 +0100

    map added w/o clusters. types are mess

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
 import { useFindTheftsQuery, useGetTheftQuery } from './generated-types';
 import { Checkbox, Box, Container } from '@material-ui/core';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
+import { formatRelative } from 'date-fns';
+import mapStyles from './mapStyles';
 
+import '@reach/combobox/styles.css';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  height: '100vh',
+  width: '100vw',
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
+};
+
+// @ts-ignore
 const App: React.FC = () => {
   const allThefts = useFindTheftsQuery();
   allThefts.startPolling(2000);
@@ -10,13 +47,180 @@ const App: React.FC = () => {
   const draftThefts = useGetTheftQuery();
   console.log(draftThefts.data);
 
+  const { isLoaded, loadError } = useLoadScript({
+    // @ts-ignore
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    // @ts-ignore
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    // @ts-ignore
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    // @ts-ignore
+    mapRef.current.panTo({ lat, lng });
+    // @ts-ignore
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return 'Error';
+  if (!isLoaded) return 'Loading...';
+
   return (
-    <Box>
-      <div style={{ marginLeft: 400, paddingTop: 50 }}>
-        <b>VIEW DRAFT NOTES</b>
-      </div>
-    </Box>
+    <div>
+      <h1>
+        Bears{' '}
+        <span role="img" aria-label="tent">
+          ⛺️
+        </span>
+      </h1>
+
+      <Locate panTo={panTo} />
+      <Search panTo={panTo} />
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            // @ts-ignore
+            key={`${marker.lat}-${marker.lng}`}
+            // @ts-ignore
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: `/bear.svg`, // @ts-ignore
+              origin: new window.google.maps.Point(0, 0), // @ts-ignore
+              anchor: new window.google.maps.Point(15, 15), // @ts-ignore
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow // @ts-ignore
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                <span role="img" aria-label="bear">
+                  🐻
+                </span>{' '}
+                Alert
+              </h2>
+              {/* @ts-ignore */}
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
   );
 };
+
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="/compass.svg" alt="compass" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      // @ts-ignore
+      location: { lat: () => 43.6532, lng: () => -79.3832 },
+      radius: 100 * 1000,
+    },
+  });
+
+  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log('😱 Error: ', error);
+    }
+  };
+
+  return (
+    <div className="search">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === 'OK' &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
 
 export default App;
